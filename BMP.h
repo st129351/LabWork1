2aadca4fb8a70ec79332cd969491a658259e059d
Lichkovaha <st129351@student.spbu.ru>
2024-12-19 08:25:22 +0000

    fix rotate

--- BMP.h
+++ BMP.h
@@ -13,8 +13,8 @@
 #pragma pack(push, 1) // must be single-byte alignment
 struct BMPFileHeader
 {
-	uint16_t file_type{0x4D42}; // _t means, this isn't variable, but data type (0x4D42 the type of bmp images)
-	uint32_t file_size{0};
+    uint16_t file_type{0x4D42}; // _t means, this isn't variable, but data type (0x4D42 the type of bmp images)
+    uint32_t file_size{0};
     uint16_t reserved1{0};
     uint16_t reserved2{0}; // default value for correct work
     uint32_t offset_data{0}; // image data offset from the beginning of the file
@@ -22,48 +22,48 @@ struct BMPFileHeader
 
 struct BMPInfoHeader // must be single-byte alignment
 {
-	uint32_t size{0}; // size of this header in bytes
-	int32_t width{0}; // width of bitmap in pixels
-	int32_t height{0}; // also height
-	// if positive, bottom-up, with origin in lower left corner
-	// if negative, top-down, with origin in upper left corner
-	uint16_t planes{1}; // the number of color planes (always 1)
-	uint16_t bit_count{0}; // the number of bit per pixel
-	uint32_t compression{0}; // the type of compression 0-3 no compression (i do without compression)
-	uint32_t size_image{0}; // the size of image in bytes
-	int32_t x_pixels_per_meter{0};
-	int32_t y_pixels_per_meter{0}; 
-	uint32_t colors_used{0}; // the num of using colors, i want use max number of colors (use 0)
-	uint32_t colors_important{0}; // if 0, all colors are required
+    uint32_t size{0}; // size of this header in bytes
+    int32_t width{0}; // width of bitmap in pixels
+    int32_t height{0}; // also height
+    // if positive, bottom-up, with origin in lower left corner
+    // if negative, top-down, with origin in upper left corner
+    uint16_t planes{1}; // the number of color planes (always 1)
+    uint16_t bit_count{0}; // the number of bit per pixel
+    uint32_t compression{0}; // the type of compression 0-3 no compression (i do without compression)
+    uint32_t size_image{0}; // the size of image in bytes
+    int32_t x_pixels_per_meter{0};
+    int32_t y_pixels_per_meter{0};
+    uint32_t colors_used{0}; // the num of using colors, i want use max number of colors (use 0)
+    uint32_t colors_important{0}; // if 0, all colors are required
 }; // second default header
 
 struct BMPColorHeader
 {
-	uint32_t red_mask{0x00ff0000}; // the color of red mask
-	uint32_t blue_mask{0x000000ff}; // the color of blue mask
-	uint32_t green_mask{0x0000ff00}; // the color of green mask
-	uint32_t alpha_mask{0xff000000}; // the alpha channel (transparency)
-	uint32_t color_space_type{0x73524742}; // the color space
-	uint32_t unused[16]{0}; // reserved data for sRGB color space (massive with 16 '0')
+    uint32_t red_mask{0x00ff0000}; // the color of red mask
+    uint32_t blue_mask{0x000000ff}; // the color of blue mask
+    uint32_t green_mask{0x0000ff00}; // the color of green mask
+    uint32_t alpha_mask{0xff000000}; // the alpha channel (transparency)
+    uint32_t color_space_type{0x73524742}; // the color space
+    uint32_t unused[16] {0}; // reserved data for sRGB color space (massive with 16 '0')
 };
 #pragma pack(pop) // no longer required single-byte alignment
 
 class BMP
 {
 public:
-	BMPFileHeader header;
-	BMPInfoHeader info_header;
-	BMPColorHeader color_header;
-	std::vector<uint8_t> data; 
+    BMPFileHeader header;
+    BMPInfoHeader info_header;
+    BMPColorHeader color_header;
+    std::vector<uint8_t> data;
 
-	BMP(const char* filename); // constructor; "char* filename" point to 1-st symbol from string, which i assign filename
-	// this allow me use char* like a string (const string in my case)
-	// compiler read char* like a string, while symbol != '\0' (end of each str)
-	void Rotate90Clockwise();
-	void Rotate90CounterClockwise();
-	void GaussianBlur();
+    BMP(const char* filename); // constructor; "char* filename" point to 1-st symbol from string, which i assign filename
+    // this allow me use char* like a string (const string in my case)
+    // compiler read char* like a string, while symbol != '\0' (end of each str)
+    void Rotate90Clockwise();
+    void Rotate90CounterClockwise();
+    void GaussianBlur();
 
-	void Save(const char* filename);
+    void Save(const char* filename);
 };
 
 #endif
--- labwork1.cpp
+++ labwork1.cpp
@@ -3,30 +3,30 @@
 //LabWork1
 #include "BMP.h"
 
-int main() 
+int main()
 {
-	try 
-	{
-		BMP image("start.bmp");
+    try
+    {
+        BMP image("start.bmp");
 
-		image.Rotate90Clockwise();
-		image.Save("Rotated90Clockwise.bmp");
+        image.Rotate90Clockwise();
+        image.Save("Rotated90Clockwise.bmp");
 
-		image.Rotate90CounterClockwise();
-		image.Save("Rotate90CounterClockwise.bmp");
+        image.Rotate90CounterClockwise();
+        image.Save("Rotate90CounterClockwise.bmp");
 
-		image.GaussianBlur();
-		image.Save("GaussianBlur.bmp");
+        image.GaussianBlur();
+        image.Save("GaussianBlur.bmp");
 
-		std::cout << "Image procecssing was successful!" << std::endl;
-		std::cout << "All modified files were created!" << std::endl;
-	}
-	catch(const std::exception &ex)
-	{
-		std::cout << "ERROR:" << std::endl;
-		std::cerr << ex.what() << std::endl;
-		return 1;
-	}
+        std::cout << "Image procecssing was successful!" << std::endl;
+        std::cout << "All modified files were created!" << std::endl;
+    }
+    catch(const std::exception &ex)
+    {
+        std::cout << "ERROR:" << std::endl;
+        std::cerr << ex.what() << std::endl;
+        return 1;
+    }
 
-	return 0;
+    return 0;
 }
